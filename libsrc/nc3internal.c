143e2c64a2a6e3c050c77ede76ce3ce4187f110d
dmh <dmh@unidata.ucar.edu>
2015-05-25 16:56:19 -0600

    1. merge latest master 2. somehow, memio.c got completely screwed up,    so fix.

--- libsrc/nc3internal.c
+++ libsrc/nc3internal.c
@@ -1584,7 +1584,7 @@ NC3_inq_type(int ncid, nc_type typeid, char *name, size_t *size)
 }
 
 /**************************************************/
-
+#if 0
 int
 NC3_set_content(int ncid, size_t size, void* memory)
 {
@@ -1610,35 +1610,7 @@ NC3_set_content(int ncid, size_t size, void* memory)
 done:
     return status;
 }
-
-/**************************************************/
-
-int
-NC3_set_content(int ncid, size_t size, void* memory)
-{
-    int status = NC_NOERR;
-    NC *nc;
-    NC3_INFO* nc3;
-
-    status = NC_check_id(ncid, &nc);
-    if(status != NC_NOERR)
-        return status;
-    nc3 = NC3_DATA(nc);
-
-#ifdef USE_DISKLESS
-    fClr(nc3->flags, NC_CREAT);
-    status = memio_set_content(nc3->nciop, size, memory);
-    if(status != NC_NOERR) goto done;
-    status = nc_get_NC(nc3);
-    if(status != NC_NOERR) goto done;
-#else
-    status = NC_EDISKLESS;
-#endif    				
-
-done:
-    return status;
-}
-
+#endif
 /**************************************************/
 
 int
